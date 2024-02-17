4cceb112c9e974376c9e4dcbe356f754a856ce2e
Tiago Fassoni <tiagofassoni@gmail.com>
2024-02-17 18:12:12 -0300

    FIxed my messed-up formatting

--- test_boggle_class.py
+++ test_boggle_class.py
@@ -2,23 +2,34 @@ from unittest import TestCase
 from boggle import Boggle
 import unittest
 
+
 class FlaskTests(TestCase):
 
-  def setUp(self):
-      pass
+    def setUp(self):
+        pass
 
-      
-  def tearDown(self):
-      pass
+    def tearDown(self):
+        pass
 
-  def test_check_invalid_word(self):
+    def test_check_invalid_word(self):
         board_object = Boggle(board_size=6)
-        self.assertEqual(board_object.check_valid_word(board_object.board, '人人人人人人'), 'not-word')
+        self.assertEqual(
+            board_object.check_valid_word(board_object.board, "人人人人人人"),
+            "not-word",
+        )
 
-  def test_check_valid_word(self):
-        board_contents = [['T', 'E', 'S', 'T', 'S'], ['T', 'E', 'S', 'T', 'S'], ['T', 'E', 'S', 'T', 'S'], ['T', 'E', 'S', 'T', 'S'], ['T', 'E', 'S', 'T', 'S']] 
+    def test_check_valid_word(self):
+        board_contents = [
+            ["T", "E", "S", "T", "S"],
+            ["T", "E", "S", "T", "S"],
+            ["T", "E", "S", "T", "S"],
+            ["T", "E", "S", "T", "S"],
+            ["T", "E", "S", "T", "S"],
+        ]
         board_object = Boggle(board_size=6, board=board_contents)
-        self.assertEqual(board_object.check_valid_word(board_object.board, 'tests'), 'ok')
+        self.assertEqual(
+            board_object.check_valid_word(board_object.board, "tests"), "ok"
+        )
 
-if __name__ == '__main__':
-    unittest.main()       
+if __name__ == "__main__":
+    unittest.main()
